aa8b590571b5c2e351d6fdd4105298ed26c59dcc
Ennin <enninfrancis47@gmail.com>
2021-02-23 03:25:53 +0000

    make it better

--- otw_api/settings.py
+++ otw_api/settings.py
@@ -168,7 +168,7 @@ s3_secret = env('s3_secret')
 
 AWS_ACCESS_KEY_ID = s3_access
 AWS_SECRET_ACCESS_KEY = s3_secret
-AWS_STORAGE_BUCKET_NAME = 'dabiara-shop'
+AWS_STORAGE_BUCKET_NAME = 'otwapi'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
